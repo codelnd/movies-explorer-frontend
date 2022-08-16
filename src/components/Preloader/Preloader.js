6dc22f1184898648edacf71e6fac8de5354d5e4c
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-08-16 19:43:59 +0700

    feat: Сверстать компонент Preloader, Movies, SavedMovies, MoviesCard, MoviesCardList

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Preloader.css'
+
+const Preloader = () => {
+    return (
+        <section className="preloader__section">
+            {/*<div className="preloader">*/}
+            {/*    <div className="preloader__container">*/}
+            {/*        <span className="preloader__round"></span>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <button className="preloader__button">Ещё</button>
+        </section>
+    )
+};
+
+export default Preloader
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './SavedMovies.css'
+import SearchForm from "../SearchForm/SearchForm";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
 
 function SavedMovies() {
     return (
         <section className="saved-movies">
-
+            <SearchForm/>
+            <MoviesCardList/>
         </section>
     );
 }
