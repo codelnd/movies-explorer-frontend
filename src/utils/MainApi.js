fd5eb905047cf78e3b7a852be2a94b9c093a7fc1
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-08-22 21:38:30 +0700

    feat: Создать описание запросов

---
+++ src/utils/MainApi.js
@@ -0,0 +1,53 @@
+export const baseUrl = 'https://api.findmovies.nomoredomains.xyz';
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    return res.json().then((data) => {
+      throw new Error(data.error || data.message);
+    });
+  }
+}
+
+export function register(name, email, password) {
+  return fetch(`${baseUrl}/signup`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email, password }),
+  }).then((res) => checkResponse(res));
+}
+
+export function login(email, password) {
+  return fetch(`${baseUrl}/signin`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => checkResponse(res));
+}
+
+export function logout(email) {
+  return fetch(`${baseUrl}/signout`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email }),
+  }).then((res) => checkResponse(res));
+}
+
+export function getUser() {
+  return fetch(`${baseUrl}/users/me`, {
+    method: 'GET',
+    credentials: 'include',
+  }).then((res) => checkResponse(res));
+}
+
+export function updateUser(name, email) {
+  return fetch(`${baseUrl}/users/me`, {
+    method: 'PATCH',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email }),
+  }).then((res) => checkResponse(res));
+}
